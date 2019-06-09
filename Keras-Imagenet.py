fde217d1ef8903293ca39dcaef32172187d77c81
Theerawat Ramchuen <ramchuen@gmail.com>
2019-06-09 19:56:34 +0700

    Web Cam Image Classification with Rasnet50

--- Keras-Imagenet.py
+++ Keras-Imagenet.py
@@ -1,72 +1,88 @@
 # Original Code
-# https://github.com/spmallick/learnopencv/blob/master/Keras-ImageNet-Models/pretrained-imagenet-models-classification.ipynb
+# https://keras.io/applications/#classify-imagenet-classes-with-resnet50
 
-import keras
+import time
+from keras.applications.resnet50 import ResNet50
+from keras.preprocessing import image
+from keras.applications.resnet50 import preprocess_input, decode_predictions
 import numpy as np
 
-from keras.applications import vgg16, inception_v3, resnet50, mobilenet
+model = ResNet50(weights='imagenet')
 
-#vgg_model = vgg16.VGG16(weights='imagenet')
-#inception_model = inception_v3.InceptionV3(weights='imagenet')
-resnet_model = resnet50.ResNet50(weights='imagenet')
-#mobilenet_model = mobilenet.MobileNet(weights='imagenet')
-
-from keras.preprocessing.image import load_img
-from keras.preprocessing.image import img_to_array
-from keras.applications.imagenet_utils import decode_predictions
-import matplotlib.pyplot as plt
-
-filename = 'cat.jpg'
-
-#In [140]:
-# load an image in PIL format
-original = load_img(filename, target_size=(224, 224))
-print('PIL image size',original.size)
-plt.imshow(original)
-plt.show()
-
-# convert the PIL image to a numpy array
-# IN PIL - image is in (width, height, channel)
-# In Numpy - image is in (height, width, channel)
-numpy_image = img_to_array(original)
-plt.imshow(np.uint8(numpy_image))
-plt.show()
-print('numpy array size',numpy_image.shape)
-
-# Convert the image / images into batch format
-# expand_dims will add an extra dimension to the data at a particular axis
-# We want the input matrix to the network to be of the form (batchsize, height, width, channels)
-# Thus we add the extra dimension to the axis 0.
-image_batch = np.expand_dims(numpy_image, axis=0)
-print('image batch size', image_batch.shape)
-plt.imshow(np.uint8(image_batch[0]))
-
-#In [142]:
-# prepare the image for the ResNet50 model
-processed_image = resnet50.preprocess_input(image_batch.copy())
-
-# get the predicted probabilities for each class
-predictions = resnet_model.predict(processed_image)
-
-# convert the probabilities to class labels
-# If you want to see the top 3 predictions, specify it using the top argument
-label_resnet = decode_predictions(predictions, top=3)
-label_resnet
-
-#In [145]:
+# Part 3 Prediction Image from video
+import numpy as np
+from keras.preprocessing import image as image_utils
+import numpy as np
 import cv2
-numpy_image = np.uint8(img_to_array(original)).copy()
-numpy_image = cv2.resize(numpy_image,(900,900))
+cap = cv2.VideoCapture(0)
 
-cv2.putText(numpy_image, "ResNet50: {}, {:.2f}".format(label_resnet[0][0][1], label_resnet[0][0][2]) , (350, 145), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
-numpy_image = cv2.resize(numpy_image, (700,700))
-cv2.imwrite("images/{}_output.jpg".format(filename.split('/')[-1].split('.')[0]),cv2.cvtColor(numpy_image, cv2.COLOR_RGB2BGR))
+while(cap.isOpened()):
+    start_t = time.time()
+    ret, frame = cap.read()
+    
+    if ret == True:
+        cv2.imshow('frame',frame)
+        cv2.imwrite('temp.jpg',frame)
+        img_path = 'temp.jpg'
+        img = image.load_img(img_path, target_size=(224, 224))
+        x = image.img_to_array(img)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+        preds = model.predict(x)
+        end_t = time.time()
+        print('Predicted:', decode_predictions(preds, top=1)[0],' Time: ',round((end_t-start_t),2))
+    
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+cap.release()
+cv2.destroyAllWindows
 
-#In [138]:
-plt.figure(figsize=[10,10])
-plt.imshow(numpy_image)
-plt.axis('off')
+exit()
 
 
 
+
+
+
+
+
+
+
+
+start_t = time.time()
+img_path = 'cat.jpg'
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+preds = model.predict(x)
+end_t = time.time()
+# decode the results into a list of tuples (class, description, probability)
+# (one such list for each sample in the batch)
+print('Predicted:', decode_predictions(preds, top=1)[0],' Time: ',round((end_t-start_t),2))
+# Predicted: [(u'n02504013', u'Indian_elephant', 0.82658225), (u'n01871265', u'tusker', 0.1122357), (u'n02504458', u'African_elephant', 0.061040461)]
+
+
+start_t = time.time()
+img_path = 'elephant.jpg'
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+preds = model.predict(x)
+end_t = time.time()
+print('Predicted:', decode_predictions(preds, top=1)[0],' Time: ',round((end_t-start_t),2))
+
+
+start_t = time.time()
+img_path = 'dog.jpg'
+img = image.load_img(img_path, target_size=(224, 224))
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+x = preprocess_input(x)
+preds = model.predict(x)
+end_t = time.time()
+print('Predicted:', decode_predictions(preds, top=1)[0],' Time: ',round((end_t-start_t),2))
 
